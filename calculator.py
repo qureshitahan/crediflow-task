75e8a358fd64c169666c76594c938d7c0f7374c0
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2025-04-25 10:19:34 +0530

    feat: add multiplication

--- calculator.py
+++ calculator.py
@@ -1,9 +1,13 @@
 class Calculator:
     def add(self, x, y):
         return x + y
-
+    
     def subtract(self, x, y):
         return x - y
+
+    def multiply(self, x, y):
+        return x * y
+
 
 if __name__ == "__main__":
     calc = Calculator()
@@ -11,3 +15,7 @@ if __name__ == "__main__":
     print("Current features: Addition, Subtraction")
     print("Example: 5 + 3 =", calc.add(5, 3))
     print("Example: 5 - 3 =", calc.subtract(5, 3))
+    print("Current features: Addition, Multiplication")
+    print("Example: 5 + 3 =", calc.add(5, 3))
+    print("Example: 5 * 3 =", calc.multiply(5, 3))
+
